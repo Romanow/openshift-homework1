b605652c0c8fb1dd16ff43d215ee1471ead058f6
galeb666 <76165103+galeb666@users.noreply.github.com>
2021-09-06 00:56:25 +0500

    Add files via upload

--- test.sh
+++ test.sh
@@ -4,32 +4,52 @@ set -e
 
 buildFrontend() {
   ./backend/gradlew clean build -p backend
-  DOCKER_BUILDKIT=1 docker build -f frontend.Dockerfile frontend/ --tag frontend:v1.0-"$STUDENT_LABEL"
+  DOCKER_BUILDKIT=1 docker build -f frontend.Dockerfile frontend/ --tag frontend-"$STUDENT_LABEL"
 }
 
 buildBackend() {
   ./backend/gradlew clean build -p backend
-  DOCKER_BUILDKIT=1 docker build -f backend.Dockerfile backend/ --tag backend:v1.0-"$STUDENT_LABEL"
+  DOCKER_BUILDKIT=1 docker build -f backend.Dockerfile backend/ --tag backend-"$STUDENT_LABEL"
 }
 
 createNetworks() {
   echo "TODO create networks"
+  docker network create --driver bridge frontend-backend  --label $BASE_LABEL-$STUDENT_LABEL
+  docker network create --driver bridge backend-postgres --label $BASE_LABEL-$STUDENT_LABEL
 }
 
 createVolume() {
   echo "TODO create volume for postgres"
+  docker volume create db-data --label $BASE_LABEL-$STUDENT_LABEL
 }
 
 runPostgres() {
   echo "TODO run postgres"
+  docker run -d \
+    -p 5432:5432  \
+    --name postgres \
+    -e POSTGRES_USER=program \
+    -e POSTGRES_PASSWORD=test \
+    -e POSTGRES_DB=todo_list \
+    --volume db-data:/var/lib/postgresql/data \
+    --volume $(pwd)/postgres:/docker-entrypoint-initdb.d/ \
+  postgres:13-alpine
+
+  docker network connect backend-postgres postgres
 }
 
 runBackend() {
   echo "TODO run backend"
+  docker run -d -p 8080:8080 -e "SPRING_PROFILES_ACTIVE=docker"  --name backend-"$STUDENT_LABEL" backend-"$STUDENT_LABEL"
+
+  docker network connect backend-postgres backend-"$STUDENT_LABEL"
+  docker network connect frontend-backend backend-"$STUDENT_LABEL"
 }
 
 runFrontend() {
   echo "RUN frontend"
+  docker run -d -p 3000:80 --name frontend-"$STUDENT_LABEL" frontend-"$STUDENT_LABEL"
+  docker network connect frontend-backend frontend-"$STUDENT_LABEL"
 }
 
 checkResult() {
@@ -46,7 +66,7 @@ checkResult() {
 
 BASE_LABEL=homework1
 # TODO student surname name
-STUDENT_LABEL=
+STUDENT_LABEL=gaponenko
 
 echo "=== Build backend backend:v1.0-$STUDENT_LABEL ==="
 buildBackend
