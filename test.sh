f4d290d9a6912da481ea70de13ce82b1a8bede0b
Khikmat Abdunabiev <abdunabiyev@gmail.com>
2022-02-05 13:21:57 +0500

    Correct test.sh and Docker files

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 set -e
 
 buildFrontend() {
-  ./backend/gradlew clean build -p backend
+  #./backend/gradlew clean build -p backend
   DOCKER_BUILDKIT=1 docker build -f frontend.Dockerfile frontend/ --tag frontend:v1.0-"$STUDENT_LABEL"
 }
 
@@ -13,31 +13,38 @@ buildBackend() {
 }
 
 createNetworks() {
-  echo "TODO create networks"
+  echo "create networks"
+  docker network create --driver bridge --label "$BASE_LABEL-$STUDENT_LABEL" network-frontend-"$STUDENT_LABEL"
+  docker network create --driver bridge --label "$BASE_LABEL-$STUDENT_LABEL" network-backend-"$STUDENT_LABEL"
 }
 
 createVolume() {
-  echo "TODO create volume for postgres"
+  echo "create volume for postgres"
+  docker volume create --label "$BASE_LABEL-$STUDENT_LABEL" volume-pg-"$STUDENT_LABEL"
 }
 
 runPostgres() {
-  echo "TODO run postgres"
+  echo "run postgres"
+  echo "docker run --name postgres --net network-backend-"$STUDENT_LABEL" -v volume-pg-"$STUDENT_LABEL":/var/lib/postgresql/data -v backend/postgres:/docker-entrypoint-initdb.d -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -d library/postgres:13-alpine"
+  docker run --name postgres --net network-backend-"$STUDENT_LABEL" -v volume-pg-"$STUDENT_LABEL":/var/lib/postgresql/data -v "$PWD"/backend/postgres:/docker-entrypoint-initdb.d -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -d library/postgres:13-alpine
 }
 
 runBackend() {
-  echo "TODO run backend"
+  echo "run backend"
+  docker run --name backend-"$BASE_LABEL-$STUDENT_LABEL" --net network-backend-"$STUDENT_LABEL" -e "SPRING_PROFILES_ACTIVE=docker" -p 8080:8080 -d backend:v1.0-"$STUDENT_LABEL" 
 }
 
 runFrontend() {
-  echo "RUN frontend"
+  echo "run frontend"
+  docker run --name frontend-"$BASE_LABEL-$STUDENT_LABEL" --net network-frontend-"$STUDENT_LABEL" -p 3000:80 -d frontend:v1.0-"$STUDENT_LABEL"
 }
 
 checkResult() {
   sleep 10
   http_response=$(
     docker exec \
-      frontend-romanow \
-      curl -s -o response.txt -w "%{http_code}" http://backend-"$STUDENT_LABEL":8080/api/v1/public/items
+      frontend-"$BASE_LABEL-$STUDENT_LABEL" \
+      curl -s -o response.txt -w "%{http_code}" http://backend-"$BASE_LABEL-$STUDENT_LABEL":8080/api/v1/public/items
   )
 
   if [ "$http_response" != "200" ]; then
@@ -48,7 +55,7 @@ checkResult() {
 
 BASE_LABEL=homework1
 # TODO student surname name
-STUDENT_LABEL=
+STUDENT_LABEL=abdunabiyev
 
 echo "=== Build backend backend:v1.0-$STUDENT_LABEL ==="
 buildBackend
