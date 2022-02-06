d6c8a1bb60cf0f75786e734653b68035f8437a1d
Abdullokh Alimov <52039078+abdullohalimov@users.noreply.github.com>
2022-02-07 00:18:26 +0500

    trying to fix test.sh

--- test.sh
+++ test.sh
@@ -38,9 +38,7 @@ runFrontend() {
 checkResult() {
   sleep 10
   http_response=$(
-    docker exec \
-      frontend-homework1-Alimov \
-      curl -s -o response.txt -w "%{http_code}" http://backend-"$STUDENT_LABEL":8080/api/v1/public/items
+    docker exec frontend-homework1-Alimov curl -v http://backend-"$STUDENT_LABEL":8080/api/v1/public/items
   )
 
   if [ "$http_response" != "200" ]; then
