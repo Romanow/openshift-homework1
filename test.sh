483fd1cac4c5f94e2bc675a38bd79645adc2b8fb
Emil Garipov <gemil@mail.ru>
2022-02-06 16:21:01 +0500

    Emil GAripov

--- test.sh
+++ test.sh
@@ -14,29 +14,43 @@ buildBackend() {
 
 createNetworks() {
   echo "TODO create networks"
+  docker network create \
+    --label "$BASE_LABEL-$STUDENT_LABEL" backend-network-"$STUDENT_LABEL" \
+    --driver bridge 
+    
+  docker network create \
+    --label "$BASE_LABEL-$STUDENT_LABEL" frontend-network-"$STUDENT_LABEL" \
+    --driver bridge    
 }
 
 createVolume() {
   echo "TODO create volume for postgres"
+  docker volume create postgres_data_volume-"$STUDENT_LABEL" \
+    --label "$BASE_LABEL-$STUDENT_LABEL"
+  docker volume create postgres_log_volume-"$STUDENT_LABEL" \
+    --label "$BASE_LABEL-$STUDENT_LABEL"   
 }
 
 runPostgres() {
   echo "TODO run postgres"
+  docker run -d --name postgres --label "$BASE_LABEL-$STUDENT_LABEL" --network backend-network-"$STUDENT_LABEL" --volume postgres_data_volume-"$STUDENT_LABEL":/var/lib/postgresql/data --volume postgres_log_volume-"$STUDENT_LABEL":/var/log/postgresql --env POSTGRES_PASSWORD=postgres --env POSTGRES_USER=postgres --env POSTGRES_DB=postgres postgres:13-alpine
 }
 
 runBackend() {
   echo "TODO run backend"
+  docker run -d --name backend-"$STUDENT_LABEL" --label "$BASE_LABEL-$STUDENT_LABEL" --network backend-network-"$STUDENT_LABEL" --env "SPRING_PROFILES_ACTIVE=docker" -p 8080:8080 backend:v1.0-"$STUDENT_LABEL"  
 }
 
 runFrontend() {
   echo "RUN frontend"
+  docker run -d --name frontend-"$STUDENT_LABEL" --label "$BASE_LABEL-$STUDENT_LABEL" --network frontend-network-"$STUDENT_LABEL" -p 3000:80 frontend:v1.0-"$STUDENT_LABEL"
 }
 
 checkResult() {
   sleep 10
   http_response=$(
     docker exec \
-      frontend-romanow \
+      frontend-Garipov_Emil \
       curl -s -o response.txt -w "%{http_code}" http://backend-"$STUDENT_LABEL":8080/api/v1/public/items
   )
 
@@ -48,7 +62,7 @@ checkResult() {
 
 BASE_LABEL=homework1
 # TODO student surname name
-STUDENT_LABEL=
+STUDENT_LABEL=Garipov_Emil
 
 echo "=== Build backend backend:v1.0-$STUDENT_LABEL ==="
 buildBackend
