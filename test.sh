684b339feeb3c102f1994b8914b3a27dd021fd2e
aromanov <aromanov@inno.tech>
2022-02-07 11:21:58 +0300

    Update test.sh and README.md

--- test.sh
+++ test.sh
@@ -36,7 +36,7 @@ checkResult() {
   sleep 10
   http_response=$(
     docker exec \
-      frontend-romanow \
+      frontend-"$STUDENT_LABEL" \
       curl -s -o response.txt -w "%{http_code}" http://backend-"$STUDENT_LABEL":8080/api/v1/public/items
   )
 
