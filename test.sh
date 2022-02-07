eab9f4b6041a91c4432de12ccc86bf89a63ca119
Saidmakhmudov <nobotir@gmail.com>
2022-02-07 14:53:10 +0500

    edited test.sh postgres and backend run

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 set -e
 
 buildFrontend() {
-  ./backend/gradlew clean build -p backend
+  #./backend/gradlew clean build -p backend
   DOCKER_BUILDKIT=1 docker build -f frontend.Dockerfile frontend/ --tag frontend:v1.0-"$STUDENT_LABEL"
 }
 
@@ -23,38 +23,49 @@ createVolume() {
   docker volume create databaseVolume --label $BASE_LABEL-$STUDENT_LABEL
 }
 
-runPostgres() {
-  echo "TODO run postgres"
-  docker run -d \
-    -p 5432:5432  \
-    --name postgres \
-    -e POSTGRES_USER=postgres \
-    -e POSTGRES_PASSWORD=postgres \
-    -e POSTGRES_DB=postgres \
-    --volume databaseVolume:/var/lib/postgresql/data \
-    --volume $(pwd)/backend/postgres:/docker-entrypoint-initdb.d/ \
-  postgres:13-alpine
+#runPostgres() {
+ # echo "TODO run postgres"
+  #docker run  -p 5432:5432 --name postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=postgres --volume databaseVolume:/var/lib/postgresql/data --volume /$(pwd)/backend/postgres:/docker-entrypoint-initdb.d/  -d postgres:13-alpine
+  #docker network connect backendTopostgres postgres
+#}
 
-  docker network connect backendTopostgres postgres
+runPostgres() {
+  echo "RUN postgres"
+  docker run -d \
+    --publish 5432:5432 \
+    --name postgres \
+    --env POSTGRES_USER=program \
+    --env POSTGRES_PASSWORD=test \
+    --env POSTGRES_DB=todo_list \
+    --volume databaseVolume:/var/lib/postgresql/data \
+    --volume /"$pwd"/backend/postgres:/docker-entrypoint-initdb.d/ \
+    --network backendTopostgres \
+    postgres:13-alpine
 }
 
-runBackend() {
-  echo "TODO run backend"
-  docker run -d -p 8080:8080 -e "SPRING_PROFILES_ACTIVE=docker"  --name backend-"$STUDENT_LABEL" backend-"$STUDENT_LABEL"
 
-  docker network connect backendTopostgres backend-"$STUDENT_LABEL"
-  docker network connect frontendTobackend backend-"$STUDENT_LABEL"
+runBackend() {
+  sleep 20
+  echo "TODO run backend"
+  docker run -d -p 8080:8080 --name backend-"$STUDENT_LABEL" \
+  --env "SPRING_PROFILES_ACTIVE=docker" \
+  --network backendTopostgres \
+  --network frontendTobackend \
+  backend:v1.0-"$STUDENT_LABEL"
+  #docker network connect backendTopostgres backend-"$STUDENT_LABEL" # zadano cherez --network
+  #docker network connect frontendTobackend backend-"$STUDENT_LABEL" # zadano cherez --network
+  
 }
 
 
 runFrontend() {
   echo "RUN frontend"
-  docker run -d -p 3000:80 --name frontend-"$STUDENT_LABEL" frontend-"$STUDENT_LABEL"
+  docker run -d -p 3000:80 --name frontend-"$STUDENT_LABEL" frontend:v1.0-"$STUDENT_LABEL"
   docker network connect frontendTobackend frontend-"$STUDENT_LABEL"
 }
 
 checkResult() {
-  sleep 20
+  sleep 5
   docker exec \
     frontend-"$STUDENT_LABEL" \
     curl -s http://backend-"$STUDENT_LABEL":8080/api/v1/public/items > /tmp/result-"$STUDENT_LABEL"
@@ -67,7 +78,7 @@ checkResult() {
 
 BASE_LABEL=homework1
 # TODO student surname name
-STUDENT_LABEL=Nobotir
+STUDENT_LABEL=nobotir
 
 echo "=== Build backend backend:v1.0-$STUDENT_LABEL ==="
 buildBackend
