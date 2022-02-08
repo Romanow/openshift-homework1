88c06032954241b475a636ed8f7ba776e6329af2
Name <username@mail.ru>
2022-02-08 17:49:40 +0500

    initial commit

--- test.sh
+++ test.sh
@@ -13,22 +13,47 @@ buildBackend() {
 }
 
 createNetworks() {
+  docker network create --driver bridge backend_frontend --label "$BASE_LABEL-$STUDENT_LABEL"
+  docker network create --driver bridge postgres_network --label "$BASE_LABEL-$STUDENT_LABEL"
   echo "TODO create networks"
 }
 
 createVolume() {
+  docker volume create postgres-data --label "$BASE_LABEL-$STUDENT_LABEL"
   echo "TODO create volume for postgres"
 }
 
 runPostgres() {
+  docker run -d \
+  --name postgres \
+  --label "$BASE_LABEL-$STUDENT_LABEL" \
+  -e POSTGRES_USER=program \
+  -e POSTGRES_PASSWORD=test \
+  --volume data_volume:/var/lib/postgresql/data \
+  --network postgres_network \
+  postgres:13
   echo "TODO run postgres"
 }
 
 runBackend() {
+  docker run -d \
+  --name backend-"$STUDENT_LABEL" \
+  --label "$BASE_LABEL-$STUDENT_LABEL" \
+  -p 8080:8080 \
+  -e SPRING_PROFILES_ACTIVE=docker \
+  --network postgres_network \
+  backend:v1.0-"$STUDENT_LABEL"
+  docker network connect backend_network backend-"$STUDENT_LABEL"
   echo "TODO run backend"
 }
 
 runFrontend() {
+  docker run -d \
+  --name frontend-"$STUDENT_LABEL" \
+  --label "$BASE_LABEL-$STUDENT_LABEL" \
+  -p 3000:80 \
+  --network backend_network \
+  frontend:v1.0-"$STUDENT_LABEL"
   echo "RUN frontend"
 }
 
@@ -36,7 +61,7 @@ checkResult() {
   sleep 10
   http_response=$(
     docker exec \
-      frontend-romanow \
+      frontend-akramov \
       curl -s -o response.txt -w "%{http_code}" http://backend-"$STUDENT_LABEL":8080/api/v1/public/items
   )
 
@@ -48,7 +73,7 @@ checkResult() {
 
 BASE_LABEL=homework1
 # TODO student surname name
-STUDENT_LABEL=
+STUDENT_LABEL=akramov
 
 echo "=== Build backend backend:v1.0-$STUDENT_LABEL ==="
 buildBackend
@@ -73,3 +98,5 @@ runFrontend
 
 echo "=== Run check ==="
 checkResult
+
+sleep 100
